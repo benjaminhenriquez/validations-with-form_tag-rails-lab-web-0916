830c163cab947dfd709df6e1a8c2ed07d2b8249a
github username <github email address>
2016-10-26 13:34:50 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,11 @@
 class Author < ActiveRecord::Base
+  validates :name, presence: true
+  # ``{ message: "must be given please" }
+  # , message: "no name"
+  validates :email, uniqueness: true
+
+  # { message: "must be given please" }
+  # , message: "email not unique"
+  validates :phone_number, length: {is: 10}
+    # message: "phone number invalid"}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 100}
+    #  message: "too short"}
+  validates :category, inclusion: ["Fiction", "Non-Fiction"]
 end
